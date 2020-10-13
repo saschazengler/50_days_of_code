9e09fa2bfa5256832f3deed9f0c36a23edf2ef5f
Sascha <hello@zenglerjs.com>
2020-10-13 18:28:36 +0200

    add day 10 asynchronous javascript

---
+++ Day_10_Asynchronous_JavaScript/script.js
@@ -0,0 +1,24 @@
+const syncButton = document.getElementById('syncButton');
+const asyncButton = document.getElementById('asyncButton');
+
+const syncText = document.getElementById('sync');
+const asyncText = document.getElementById('async');
+
+
+function syncDisplay() {
+    syncText.innerText = 'I run synchronous.';
+    asyncText.innerText = 'Me too.';
+};
+
+
+function asyncDisplay() {
+    syncText.innerText = 'I\'m already here.'
+
+    setTimeout(() => {
+        asyncText.innerText = 'I waited 1 second for being print out.';
+    }, 1000);
+};
+
+
+syncButton.addEventListener('click', () => syncDisplay());
+asyncButton.addEventListener('click', () => asyncDisplay());
