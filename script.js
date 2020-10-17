b45e7ace2c74667382b7a4dc28838eb606bb9a76
Sascha <hello@zenglerjs.com>
2020-10-17 18:23:32 +0200

    fixes github pages routing

--- script.js
+++ script.js
@@ -2,59 +2,59 @@ const projectContainer = document.getElementById('projectContainer');
 const projects = [ undefined,
     {
         title: 'Countdown',
-        link: '/Day_1_Countdown'
+        link: '/100_days_of_code/Day_1_Countdown'
     },
     { 
         title: 'Contact Form',
-        link: '/Day_2_Contact_Form'
+        link: '/100_days_of_code/Day_2_Contact_Form'
     },
     {
         title: 'Navigation Menu',
-        link: '/Day_3_Navigation_Menu'
+        link: '/100_days_of_code/Day_3_Navigation_Menu'
     },
     {
         title: 'Invisible Touch',
-        link: '/Day_4_Invisible_Touch'
+        link: '/100_days_of_code/Day_4_Invisible_Touch'
     },
     {   
         title: 'Pricing Design',
-        link: '/Day_5_Pricing_design'
+        link: '/100_days_of_code/Day_5_Pricing_design'
     },
     {
         title: 'zsh Quiz',
-        link: '/Day_6_Quiz'
+        link: '/100_days_of_code/Day_6_Quiz'
     },
     {
         title: 'Password Generator',
-        link: '/Day_7_Password_Generator'
+        link: '/100_days_of_code/Day_7_Password_Generator'
     },
     {
         title: 'Jan & Oli',
-        link: '/Day_8_Jan_und_Oli'
+        link: '/100_days_of_code/Day_8_Jan_und_Oli'
     },
     {
         title: 'Night Mode',
-        link: '/Day_9_Night_Mode'
+        link: '/100_days_of_code/Day_9_Night_Mode'
     },
     {
         title: 'Asynchronous JavaScript',
-        link: '/Day_10_Asynchronous_JavaScript'
+        link: '/100_days_of_code/Day_10_Asynchronous_JavaScript'
     },
     {
         title: 'Local Storage',
-        link: '/Day_11_Local_Storage'
+        link: '/100_days_of_code/Day_11_Local_Storage'
     },
     {
         title: 'Minimal Text Input',
-        link: '/Day_12_Minimal_Text_Input'
+        link: '/100_days_of_code/Day_12_Minimal_Text_Input'
     },
     {
         title: 'Silverstein Music Videos',
-        link: '/Day_13_Silverstein_Musicvideos'
+        link: '/100_days_of_code/Day_13_Silverstein_Musicvideos'
     },
     {
         title: 'Overview Site',
-        link: '/Day_14_Overview_Site'
+        link: '/100_days_of_code/Day_14_Overview_Site'
     }
 ];
 
