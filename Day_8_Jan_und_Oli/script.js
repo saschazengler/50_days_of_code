3200e761c8b8ddee9c5e61f7b228ef235d2ec4dd
Sascha <hello@zenglerjs.com>
2020-10-20 12:45:52 +0200

    fixes viewport height on mobile devices

--- Day_8_Jan_und_Oli/script.js
+++ Day_8_Jan_und_Oli/script.js
@@ -1,3 +1,6 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const body = document.getElementById('body');
 const text = document.getElementById('text');
 
