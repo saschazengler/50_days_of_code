131660debd888470add5060178addde381587279
Sascha <hello@zenglerjs.com>
2020-10-22 21:08:51 +0100

    add day 19 location access

--- Day_19_Location_Access/script.js
+++ Day_19_Location_Access/script.js
@@ -1,31 +1,33 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const body = document.getElementsByTagName('body')[0];
 const button = document.getElementsByTagName('button')[0];
 
 let latitude = 0;
 let longitude = 0;
 
-
-function getCurrentLocation() {
-    navigator.geolocation.getCurrentPosition(setPosition);
+function getPosition() {
+    navigator.geolocation.getCurrentPosition(setDegrees);
 };
 
-
-function setPosition(position) {
-    latitude = position.coords.latitude;
+function setDegrees(position) {
     longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
 
-    showLocation();
+    displayPosition();
 };
 
-
-function showLocation() {
+function displayPosition() {
     button.style.display = 'none';
-    
-    const node = document.createElement('iframe');
-    node.setAttribute('frameborder', '0');
-    node.classList.add('map');
-    node.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyAAzcvpUuLyLRYhK3rLWlgnKz-GEYbKfEY&center=${latitude},${longitude}&zoom=15`;
-    body.appendChild(node);
+
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('frameborder', '0');
+    iframe.classList.add('map');
+
+    iframe.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyBnMXGdr3RVTRGv7cG68NRoCNbFxqRkuiQ&center=${latitude},${longitude}&zoom=15`;
+
+    body.appendChild(iframe);
 };
 
-button.addEventListener('click', () => getCurrentLocation());
+button.addEventListener('click', () => getPosition());
