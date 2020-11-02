000ddd9d1867f760eeb11ed6d4d7357ff151adb6
Sascha Zengler <zengler@me.com>
2020-11-02 01:12:34 +0100

    add day 30 daily nasa

--- Day_30_Daily_Nasa/script.js
+++ Day_30_Daily_Nasa/script.js
@@ -1,7 +1,8 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-const content = document.getElementById('content');
+const image = document.getElementById('image');
+const video = document.getElementById('video');
 const url = 'https://api.nasa.gov/planetary/apod?api_key=mbJZtBfrohIEyCSdAcbcCx8ulk32avNXy9FFhtcc';
 
 
@@ -10,9 +11,20 @@ async function fetchApi() {
     .then(response => response.json())
     .then(data => {
         alert(data.title);
-        content.setAttribute('src', data.url);
+        validateUrl(data);
     })
     .catch(error => console.error(error));
 };
 
 fetchApi(); 
+
+
+function validateUrl(data) {
+    let url = data.url;
+    
+    if (url.includes('.jpg') || url.includes('.png')) {
+        image.setAttribute('src', url);
+    } else {
+        video.setAttribute('src', url);
+    };
+};
